28c8c0282cf34d123430345f968f31c9bdb2e42f
Aditya5489 <ap5663019@gmail.com>
2026-02-19 21:21:23 +0530

    Frontend url changes

--- index.js
+++ index.js
@@ -1,38 +1,60 @@
 require("dotenv").config();
-const express= require("express");
-const cors=require("cors");
-const path=require("path");
-const app=express();
-const connectDB=require('./config/db');
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const app = express();
+const connectDB = require("./config/db");
 
-const authRoutes=require("./routes/authRoutes");
-const sessionRoutes=require("./routes/sessionRoutes");
-const questionRoutes=require("./routes/questionRoutes");
-const resumeRoutes=require('./routes/resumeRoutes')
+const authRoutes = require("./routes/authRoutes");
+const sessionRoutes = require("./routes/sessionRoutes");
+const questionRoutes = require("./routes/questionRoutes");
+const resumeRoutes = require("./routes/resumeRoutes");
 
-const {protect}=require("./middlewares/authMiddleware");
-const {generateInterviewQuestions,generateConceptExplaination}=require("./controllers/aiController");
+const { protect } = require("./middlewares/authMiddleware");
+const {
+  generateInterviewQuestions,
+  generateConceptExplaination,
+} = require("./controllers/aiController");
 
-app.use(cors({
-    origin:"*",
-    method:['GET','POST','PUT','DELETE'],
-    allowHeaders:['Content-Type','Authorization']
-}));
 
 connectDB();
 
+
 app.use(express.json());
 
-app.use("/api/auth",authRoutes);
-app.use("/api/questions",questionRoutes);
-app.use("/api/sessions",sessionRoutes);
-app.use('/api/resume', resumeRoutes);
+
+const allowedOrigins = [
+  "http://localhost:5173", // Vite local
+  "http://localhost:3000", // CRA local
+  "https://ace-prep-frontend.vercel.app", // Production frontend
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
 
-app.post("/api/ai/generate-questions",protect,generateInterviewQuestions);
-app.post("/api/ai/generate-explanation",protect,generateConceptExplaination);
+app.use("/api/auth", authRoutes);
+app.use("/api/questions", questionRoutes);
+app.use("/api/sessions", sessionRoutes);
+app.use("/api/resume", resumeRoutes);
+
+app.post("/api/ai/generate-questions", protect, generateInterviewQuestions);
+app.post("/api/ai/generate-explanation", protect, generateConceptExplaination);
 
 
-
-const PORT=process.env.PORT || 5000;
-app.listen(PORT,()=>console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
